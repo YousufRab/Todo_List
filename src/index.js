0534f0944fa8e3e87fdbb0bae7d842fa0cf3dc25
Yousuf Rabani <m.yousuf.rabani@gmail.com>
2022-11-23 12:45:37 -0800

    Create class todo and import style.css

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import './style.css';
+
+// Todo object
+
+class Todo {
+
+    constructor(title, description, dueDate, priority) {
+        this.title = title;
+        this.description = description;
+        this.date = dueDate;
+        this.priority = priority;
+    }
+
+}
