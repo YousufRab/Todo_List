e02a880220f643ebf44eff391c66ada4e3e8fd32
Yousuf Rabani <m.yousuf.rabani@gmail.com>
2022-11-24 11:02:13 -0800

    Remove clean:true from output for now

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,7 @@ module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
+        path: path.resolve(__dirname, 'dist')
     },
     devtool: 'inline-source-map',
     devServer: {
