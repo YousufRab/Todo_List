937d4baeec4f99f9408f06b802e74371e1da23a5
Yousuf Rabani <m.yousuf.rabani@gmail.com>
2022-11-26 10:49:07 -0800

    Add boolean instance property 'completed' to Todo class constructor

--- src/index.js
+++ src/index.js
@@ -1,19 +1,27 @@
 import './style.css';
-import {pageContent} from './pageContent';
+import {pageContent, projects} from './pageContent';
 
 const homeContent = document.querySelector('#homeContent');
 homeContent.append(pageContent());
+
+projects.addToProList('King shit');
+projects.render();
 
 
 // Todo object
 
 class Todo {
 
-    constructor(title, description, dueDate, priority) {
+    constructor(title, description, dueDate, priority, completed) {
         this.title = title;
         this.description = description;
         this.date = dueDate;
         this.priority = priority;
+        this.completed = completed;
     }
 
 }
+
+// watch these vids:
+// https://www.youtube.com/watch?v=W7FaYfuwu70&ab_channel=WebDevSimplified
+// https://www.youtube.com/watch?v=WgFkdGqC6ng&ab_channel=MiladTech
