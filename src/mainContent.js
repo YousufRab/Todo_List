50b6fcc4a075013ea944cb956c17470a57353611
Yousuf Rabani <m.yousuf.rabani@gmail.com>
2022-11-26 12:04:12 -0800

    Move radio button creation code out of createToDo card, todo priority will be displayed via a background color in the div displaying todo information
    Create prioLevel function which will return a color based on the priority level of todo object

--- src/mainContent.js
+++ src/mainContent.js
@@ -5,6 +5,13 @@ const mainContent = () => {
 
         const todoCard = document.createElement('div');
         todoCard.classList.add('todoCard');
+        todoCard.style.backgroundColor = prioLevel(todo);
+
+        const mainInfo = document.createElement('div');
+        mainInfo.classList.add('mainInfo');
+     
+
+        todoCard.append(mainInfo);
         
         const title = document.createElement('div');
         title.classList.add('todoTitle');
@@ -18,31 +25,41 @@ const mainContent = () => {
         dueDate.classList.add('todoDate');
         //dueDate.innerHTML = dateToString(todo.dueDate);
     
-        const lowPriority = document.createElement('input');
-        lowPriority.setAttribute('type', 'radio');
-        lowPriority.setAttribute('name', 'priority');
-        lowPriority.id = 'lowPrio';
-        lowPriority.classList.add('prioBtn');
-        const lowLabel = document.createElement('label');
-        lowLabel.setAttribute('for', 'lowPrio');
-        lowLabel.innerHTML = 'Low';
-
-        const medPriority = document.createElement('input');
-        medPriority.setAttribute('type', 'radio');
-        medPriority.setAttribute('name', 'priority');
-        medPriority.id = 'medPrio';
-        medPriority.classList.add('prioBtn');
-        const medLabel = document.createElement('label');
-        medLabel.setAttribute('for', 'medPrio');
-        medLabel.innerHTML = 'Medium';
-
-        const highPriority = document.createElement('input');
-        highPriority.setAttribute('type', 'radio');
-        highPriority.setAttribute('name', 'priority');
-        highPriority.id = 'highPrio';
-        highPriority.classList.add('prioBtn');
-        const highLabel = document.createElement('label');
-        highLabel.setAttribute('for', 'highPrio');
-        highLabel.innerHTML = 'High';
     }
+
+    function prioLevel (todo) {
+        
+    }
+
 }
+
+
+//Radio button selected depends on todo.priority choice, need a fxn to convert
+//This code will be used later to create todo radio buttons
+const lowPriority = document.createElement('input');
+lowPriority.setAttribute('type', 'radio');
+lowPriority.setAttribute('name', 'priority');
+lowPriority.id = 'lowPrio';
+lowPriority.classList.add('prioBtn');
+const lowLabel = document.createElement('label');
+lowLabel.setAttribute('for', 'lowPrio');
+lowLabel.innerHTML = 'Low';
+
+const medPriority = document.createElement('input');
+medPriority.setAttribute('type', 'radio');
+medPriority.setAttribute('name', 'priority');
+medPriority.id = 'medPrio';
+medPriority.classList.add('prioBtn');
+const medLabel = document.createElement('label');
+medLabel.setAttribute('for', 'medPrio');
+medLabel.innerHTML = 'Medium';
+
+const highPriority = document.createElement('input');
+highPriority.setAttribute('type', 'radio');
+highPriority.setAttribute('name', 'priority');
+highPriority.id = 'highPrio';
+highPriority.classList.add('prioBtn');
+const highLabel = document.createElement('label');
+highLabel.setAttribute('for', 'highPrio');
+highLabel.innerHTML = 'High';
+    
