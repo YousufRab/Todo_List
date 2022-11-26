8e1e9cf97fcd437266aa5ad621a8ea34d8271438
Yousuf Rabani <m.yousuf.rabani@gmail.com>
2022-11-26 10:40:29 -0800

    Create label element for lowPriority radio button and set the 'for' attribute to link it to radio button

--- src/mainContent.js
+++ src/mainContent.js
@@ -23,11 +23,20 @@ const mainContent = () => {
         lowPriority.setAttribute('name', 'priority');
         lowPriority.id = 'lowPrio';
         lowPriority.classList.add('prioBtn');
+        const lowLabel = document.createElement('label');
+        lowLabel.setAttribute('for', 'lowPrio');
+        lowLabel.innerHTML = 'Low';
 
         const medPriority = document.createElement('input');
         medPriority.setAttribute('type', 'radio');
         medPriority.setAttribute('name', 'priority');
         medPriority.id = 'medPrio';
         medPriority.classList.add('prioBtn');
+
+        const highPriority = document.createElement('input');
+        highPriority.setAttribute('type', 'radio');
+        highPriority.setAttribute('name', 'priority');
+        highPriority.id = 'highPrio';
+        highPriority.classList.add('prioBtn');
     }
 }
