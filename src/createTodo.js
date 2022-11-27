95bde9e206a939875a625e0a41315f6cda8fda7c
Yousuf Rabani <m.yousuf.rabani@gmail.com>
2022-11-27 11:01:06 -0800

    Create new module createTodo.js for creation of todo input form, collecting input form data and outputing a todocard on the DOm

---
+++ src/createTodo.js
@@ -0,0 +1,17 @@
+
+
+const createTodo = () => {
+
+    class Todo {
+
+        constructor(title, description, dueDate, priority, completed) {
+            this.title = title;
+            this.description = description;
+            this.date = dueDate;
+            this.priority = priority;
+            this.completed = completed;
+        }
+    }
+
+    
+}
