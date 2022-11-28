73fc4b42891b30a5614089ccffc8ecddeac19b85
Yousuf Rabani <m.yousuf.rabani@gmail.com>
2022-11-28 15:22:05 -0800

    Delete redundant lines of code

--- src/mainContent.js
+++ src/mainContent.js
@@ -68,32 +68,5 @@ export const mainContent = (() => {
 })();
 
 
-//Radio button selected depends on todo.priority choice, need a fxn to convert
-//This code will be used later to create todo radio buttons
-const lowPriority = document.createElement('input');
-lowPriority.setAttribute('type', 'radio');
-lowPriority.setAttribute('name', 'priority');
-lowPriority.id = 'lowPrio';
-lowPriority.classList.add('prioBtn');
-const lowLabel = document.createElement('label');
-lowLabel.setAttribute('for', 'lowPrio');
-lowLabel.innerHTML = 'Low';
 
-const medPriority = document.createElement('input');
-medPriority.setAttribute('type', 'radio');
-medPriority.setAttribute('name', 'priority');
-medPriority.id = 'medPrio';
-medPriority.classList.add('prioBtn');
-const medLabel = document.createElement('label');
-medLabel.setAttribute('for', 'medPrio');
-medLabel.innerHTML = 'Medium';
-
-const highPriority = document.createElement('input');
-highPriority.setAttribute('type', 'radio');
-highPriority.setAttribute('name', 'priority');
-highPriority.id = 'highPrio';
-highPriority.classList.add('prioBtn');
-const highLabel = document.createElement('label');
-highLabel.setAttribute('for', 'highPrio');
-highLabel.innerHTML = 'High';
     
