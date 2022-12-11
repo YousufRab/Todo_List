fab51c621fb2103c19d08f0dcce93539a8d0bb09
Yousuf Rabani <m.yousuf.rabani@gmail.com>
2022-12-11 14:22:28 -0800

    add line for adding click even listener to 'This week' sidebar link

--- src/index.js
+++ src/index.js
@@ -20,11 +20,14 @@ console.table(projects.projectList);
 
 //testing home button sidebar - delete after testing is done
 //create 3 todo objects
-const todo1 = new createTodo.Todo('Eat', 'Stuff your face', '2022-12-10', 'low', false, 1, 'Achieve enlightenment');
+const todo1 = new createTodo.Todo('Eat', 'Stuff your face', '2022-12-11', 'low', false, 1, 'Achieve enlightenment');
 const todo2 = new createTodo.Todo('Pray', 'Prayyy to yo lord', '2024-01-18', 'medium', false, 2, 'Fana');
-const todo3 = new createTodo.Todo('Love','Come onn','2028-12-09', 'high', false, 3, 'do fuck all');
+const todo3 = new createTodo.Todo('Love','Come onn','2022-12-14', 'high', false, 3, 'do fuck all');
 createTodo.todoList.push(todo1, todo2, todo3);
 // 
+
+//testing incrementDay function
+console.log(sideBarLinks.incrementDay(3));
 
 const homeBtnLink = document.querySelector('#homeLink');
 homeBtnLink.addEventListener('click', sideBarLinks.homeBtn);
@@ -32,7 +35,8 @@ homeBtnLink.addEventListener('click', sideBarLinks.homeBtn);
 const todayBtnLink = document.querySelector('#todayLink');
 todayBtnLink.addEventListener('click', sideBarLinks.todayBtn);
 
-
+const weekLink = document.querySelector('#weekLink');
+weekLink.addEventListener('click', sideBarLinks.weekBtn);
 
 
 
