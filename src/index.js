7f4057fc5ed09e304067fd629ce107411c0f1c93
Yousuf Rabani <m.yousuf.rabani@gmail.com>
2022-12-15 11:43:41 -0800

    Remove redundant line adding event listener to DOM element

--- src/index.js
+++ src/index.js
@@ -9,16 +9,7 @@ homeContent.append(createTodo.todoForm());
 
 homeContent.append(pageContent());
 
-//testing home button sidebar - delete after testing is done
-//create 3 todo objects
-const todo1 = new createTodo.Todo('Eat', 'Stuff your face', '2022-12-11', 'low', false, 1, 'Achieve enlightenment');
-const todo2 = new createTodo.Todo('Pray', 'Prayyy to yo lord', '2024-01-18', 'medium', false, 2, 'Fana');
-const todo3 = new createTodo.Todo('Love','Come onn','2022-12-14', 'high', false, 3, 'do fuck all');
-createTodo.todoList.push(todo1, todo2, todo3);
-// 
 
-const weekLink = document.querySelector('#weekLink');
-weekLink.addEventListener('click', sideBarLinks.weekBtn);
 
 
 
