0e48b3ab2e9ccf6d342e33761caf4c07eadf1be5
Yousuf Rabani <m.yousuf.rabani@gmail.com>
2022-12-19 16:19:34 -0800

    Add two if conditions in the click event listener function for overlay

--- src/index.js
+++ src/index.js
@@ -10,20 +10,32 @@ homeContent.append(createTodo.todoForm());
 
 
 homeContent.append(pageContent());
+////////
+
 let confirmEditBtn = document.getElementById('conEditBtn');
 confirmEditBtn.addEventListener('click', mainContent.confirmEditBtn);
 
+let detailsContainer = document.getElementById('detailsContainer');
+let editContainer = document.getElementById('editContainer');
 let overlay = document.getElementById('overlay');
+
 overlay.addEventListener('click', ()=> {
+    mainContent.switchOverlay();
+    if (detailsContainer.classList.contains('detailsVisible')) {
+        mainContent.showDetailsSwitch();
+    };
+    if (editContainer.classList.contains('editContainer-visible')) {
+        mainContent.editContainerSwitch();
+    };
+});
+
+let closeDetails = document.getElementById('closeDetails');
+closeDetails.addEventListener('click', () => {
     mainContent.switchOverlay();
     mainContent.showDetailsSwitch();
 });
-let closeDetails = document.getElementById('closeDetails');
-closeDetails.addEventListener('click', () => {
 
-    mainContent.switchOverlay();
-    mainContent.showDetailsSwitch();
-})
+
 
 
 
